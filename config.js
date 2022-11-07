5fcc5818edaef2f2352ed525cf53ff0594ac1ba2
phmanh061101 <81797161+phmanh061101@users.noreply.github.com>
2022-11-08 02:14:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/le.phuongthao0603';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng cô giáo của tui';
+const contentCard = 'Chúc cô giáo của anh 20/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau <br><br><br> From SG with Love';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+
+const giftImage = 'pthao.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
